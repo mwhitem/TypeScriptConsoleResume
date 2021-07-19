b3ab26f8635056dc83ef98e425231e765e416f39
Madison White <mvwhite32@gmail.com>
2021-07-19 13:29:39 -0500

    commit

---
+++ app.js
@@ -0,0 +1,45 @@
+"use strict";
+// descriptor object
+var myDiscription = {
+    name: "Madison White",
+    career: "Full Stack Student",
+    description: "I like to learn new things",
+};
+console.log("Name: " + myDiscription.name.toUpperCase());
+console.log("Career: " + myDiscription.career);
+console.log("Description: " + myDiscription.description);
+// interests array
+console.log("\nMy Interests:");
+var myInterests = [
+    "Coding",
+    "Long walks on the beach",
+    "Music",
+    "Rollercoasters",
+];
+for (var index = 0; index < myInterests.length; index++)
+    if (myInterests[index]) {
+        console.log("* " + myInterests[index]);
+    }
+// prev experience
+console.log("\nMy Previous Experience:");
+function displayPosition(jobName, jobTitle, jobDescription) {
+    console.log("* " + jobName + jobTitle + ": " + jobDescription);
+}
+displayPosition("NASA ", "Astronaut ", "I went to space, studied rocket science, and ate space food.");
+displayPosition("UN ", "Diplomat ", "I dealt with international conflicts, spoke french, and held meetings.");
+displayPosition("Atlantic Records ", "Pop Star ", "I sang pop songs, toured the world, and performed at the Grammys.");
+// my skills
+function displaySkill(skillName, isCool) {
+    if (isCool == true) {
+        console.log("BAM: " + skillName);
+    }
+    else {
+        console.log(skillName);
+    }
+}
+console.log("\nMy Skills:");
+displaySkill("French", true);
+displaySkill("HTML", false);
+displaySkill("Ballet", true);
+displaySkill("Sewing", false);
+displaySkill("Word", false);
